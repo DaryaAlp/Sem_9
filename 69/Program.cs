5b50b1e06183b02cdca910b03a0ec97a31eac402
Darya <d.a_alpatskaya@mail.ru>
2023-04-22 14:15:45 +0300

    Добавили задачи 63, 65,67,69

---
+++ 69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+Write("Введите число степени: ");
+int pow = int.Parse(ReadLine());
+
+WriteLine($"{PowNumbers(number, pow)}");
+int PowNumbers(int number, int pow)
+{
+if (pow == 0)
+return 1;
+return PowNumbers (number, --pow)*number;
+}
